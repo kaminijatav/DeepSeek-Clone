11c617c45ac9ae72c78aeb496106ba9f3a47e040
kaminijatav <148079846+kaminijatav@users.noreply.github.com>
2025-07-08 00:00:13 +0530

    Update page.tsx

--- client/src/app/chat/[id]/page.tsx
+++ client/src/app/chat/[id]/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 import Conversations from '../Conversations';
 
-// export default  function Page ({params} : {params: {id:string}}) {
-//     const {id} = await params;
-//     return <Conversations id={id}/>
-// }
-export default async function Page({ params }: { params: { id: string } }) {
-    const { id } =  params;
-    return <Conversations id={id} />;
+export default function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  return <Conversations id={id} />;
 }
